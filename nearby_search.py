24ad9dae6c0be89518fbc3c3ded66f70e539c3ad
SadNarwhal27 <schallt2015@gmail.com>
2023-06-15 08:44:05 -0400

    Fixed web formatting and linted code

--- nearby_search.py
+++ nearby_search.py
@@ -1,13 +1,19 @@
+"""Creates the Google class for finding nearby restaurants"""
 
-import googlemaps, json, random, time
+import random
+import time
+# import json
+
+import googlemaps
 from decouple import config
 
 class Google():
-    """Uses the Google Maps API to grab restaurants within a certain radius and select a random restaurant from the data."""
+    """Uses the Google Maps API to grab restaurants within a certain radius and select a random 
+    restaurant from the data."""
 
     def __init__(self):
         """Initializes the maps client that all API requests use"""
-        
+
         self.gmaps = googlemaps.Client(key=config('API_KEY'))
 
 
@@ -15,65 +21,78 @@ class Google():
         """Converts miles to meters"""
 
         return int(abs(miles) * 1609.34)
-    
+
+
     def _trim_data(self, data):
         """Removes restaurants from file based on type tags"""
 
-        TYPES_TO_REMOVE = ['gas_station', 'spa'] # Removal tags
+        types_to_remove = ['gas_station', 'spa'] # Removal tags
 
         for place in data:
-            if any(tag in TYPES_TO_REMOVE for tag in place['types']):
+            if any(tag in types_to_remove for tag in place['types']):
                 data.remove(place)
 
         return data
-    
+
+
     def _set_coordinates(self, location):
         """Checks if a given location are a set of coordinates and makes them that if necessary"""
 
-        if type(location) != tuple:
+        if not isinstance(location, tuple):
             coordinates = self.geocode_location(location)
         else:
-            coordinates = tuple([round(num, 4) for num in location]) # Turn into helper function
+            coordinates = tuple(round(num, 4) for num in location) # Turn into helper function
 
         return coordinates
-    
-    def _get_response(self, location, radius, type, keyword=None, open_now=True, page_token=None, min_price=None, max_price=None):
+
+
+    def _get_response(self, location, radius, place_type, keyword=None,
+                      open_now=True, page_token=None, min_price=None, max_price=None):
         """Creates a places_nearyby request from provided parameters"""
+
         response = self.gmaps.places_nearby(
             location=location,
             radius=radius,
             keyword=keyword,
-            min_price=None,
-            max_price=None,
+            min_price=min_price,
+            max_price=max_price,
             open_now=open_now,
-            type=type,
+            type=place_type,
             page_token=page_token
         )
         return response
-    
-    def _get_photos(self, photo_reference, maxwidth=1000):
-        PHOTOS_URL = "https://maps.googleapis.com/maps/api/place/photo?&photo_reference="
-        url = PHOTOS_URL + photo_reference + f"&maxwidth={maxwidth}&key=" + config('API_KEY')
+
+
+    def _get_photos(self, photo_reference, maxwidth=500):
+        """Gets the first photo associated with a place from Google"""
+
+        photos_url = "https://maps.googleapis.com/maps/api/place/photo?&photo_reference="
+        url = photos_url + photo_reference + f"&maxwidth={maxwidth}&key=" + config('API_KEY')
         return url
+
 
     def geocode_location(self, location):
         """Converts a place/address/Place ID into coordinates"""
-
-        geocode_results = tuple([round(num, 4) for num in self.gmaps.geocode(location)[0]['geometry']['location'].values()])
+        coordinates = self.gmaps.geocode(location)[0]['geometry']['location'].values()
+        geocode_results = tuple(round(num, 4) for num in coordinates)
         return geocode_results
+
 
     def reverse_geocode_location(self, coordinates):
         """Converts a tuple of coordinates to an address"""
 
         reverse_geocode_results = self.gmaps.reverse_geocode(coordinates)[0]['formatted_address']
-        return reverse_geocode_results 
-    
+        return reverse_geocode_results
+
+
     def geolocate(self):
+        """WIP Geolocation function"""
+
         return self.gmaps.geolocate()
-    
+
+
     def nearby_search(self, location, distance=5, keyword=None):
         """Creates a maps Places API request to find nearby restaurants"""
-
         # Makes sure a location tuple is passed into the response request
         coordinates = self._set_coordinates(location)
 
@@ -85,29 +104,29 @@ class Google():
 
         # Pulls place data using an API request
         response = self._get_response(
-            location=coordinates, 
-            radius=distance_in_meters, 
-            open_now=open_now, 
-            type=place_type, 
+            location=coordinates,
+            radius=distance_in_meters,
+            open_now=open_now,
+            place_type=place_type,
             keyword=keyword)
 
-        if keyword != None:
+        if keyword is not None:
             data = self._trim_data(response['results'])
         else:
-            data = self._trim_data(response['results'])[:result_limiter] # Limits the number of results
+            data = self._trim_data(response['results'])[:result_limiter] # Limits total results
         next_page_token = response.get('next_page_token')
 
         # Goes to the next page of results from the request until everything is grabbed
         while next_page_token:
             time.sleep(2) # Prevents repeat data
             response = self._get_response(
-                location=coordinates, 
-                radius=distance_in_meters, 
-                open_now=open_now, 
-                type=place_type, 
+                location=coordinates,
+                radius=distance_in_meters,
+                open_now=open_now,
+                place_type=place_type,
                 keyword=keyword,
                 page_token=next_page_token)
-            
+
             data.extend(self._trim_data(response.get('results')))
             next_page_token = response.get('next_page_token')
 
@@ -120,6 +139,6 @@ class Google():
         try:
             pick['photos'] = self._get_photos(pick['photos'][0]['photo_reference'])
         except:
-            pass
+            print('No photo associated with place')
 
         return pick
